0ee4315b7938bb94ad94a7bf1178791fd963346f
GANIDAGA <112988994+GANIDAGA@users.noreply.github.com>
2023-05-31 13:53:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ const app = express();
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const cors = require("cors");
 app.use(express.json());
 
 app.use(cors());
 app.use(router);
 
+const PORT = process.env.PORT || 8000;
+
 const uri = mongoose
   .connect("mongodb://mongo:FXeqSCKeBcblQ4dbkUuP@containers-us-west-26.railway.app:6778")
   .then(() => console.log("db started"));
 
-app.listen(8000, () => {
-  console.log("server started on port 8000");
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
